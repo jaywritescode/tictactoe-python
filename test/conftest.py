ae040c19dac241a08623ff5b52726e0e922efbbc
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-23 11:01:25 -0400

    test that you can't play twice in the same square

---
+++ test/conftest.py
@@ -0,0 +1,17 @@
+from more_itertools import consume
+import pytest
+from src.players import KeyboardInterfacePlayer
+
+from src.tictactoe import TicTacToe
+
+
+@pytest.fixture
+def initial_state():
+    return TicTacToe()
+
+@pytest.fixture
+def after_one_ply():
+    game = TicTacToe(playerO_class=KeyboardInterfacePlayer)
+    game.board[2,1] = game.playerX
+    consume(game.players, 1)
+    return game
--- test/test_keyboard_interface_player.py
+++ test/test_keyboard_interface_player.py
@@ -27,3 +27,36 @@ def test_player_one_get_move(input_value, expected_index):
 
         move = tictactoe.playerX.get_move()
         assert_that(move).has_player(tictactoe.playerX).has_selection(expected_index)
+
+@pytest.mark.parametrize(
+    "input_value, expected_index",
+    [
+        ("1,1", (0, 0)),
+        ("1,2", (0, 1)),
+        ("1 3", (0, 2)),
+        ("2,1", (1, 0)),
+        ("2,2", (1, 1)),
+        ("2,3", (1, 2)),
+        ("3,1", (2, 0)),
+        # ("3,2", (2, 1)),  already played
+        ("3,3", (2, 2)),
+    ],
+)
+def test_player_two_get_move(after_one_ply, input_value, expected_index):
+    with mock.patch("src.players.input", create=True) as mock_input:
+        tictactoe = after_one_ply
+        mock_input.return_value = input_value
+
+        move = tictactoe.playerO.get_move()
+        assert_that(move).has_player(tictactoe.playerO).has_selection(expected_index)
+
+
+def test_cannot_play_at_occupied_coordinate(after_one_ply):
+    with mock.patch("src.players.input", create=True) as mock_input:
+        tictactoe = after_one_ply
+        mock_input.side_effect = ["3,2", "1,1"]
+
+        move = tictactoe.playerO.get_move()
+        assert_that(mock_input.call_count).is_equal_to(2)
+        assert_that(move).has_selection((0,0))
+
