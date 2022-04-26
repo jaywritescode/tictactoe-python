1f8f0077ea433a6ba01ff0a3975d95deac54f07b
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-26 18:00:15 -0400

    Board.board -> Board.squares

--- src/board.py
+++ src/board.py
@@ -19,21 +19,21 @@ class Board:
     ]
     DIAGONALS = [[(0, 0), (1, 1), (2, 2)], [(0, 2), (1, 1), (2, 0)]]
 
-    def __init__(self, board=None):
-        if board is None:
-            self.board = [[None for col in range(3)] for row in range(3)]
+    def __init__(self, squares=None):
+        if squares is None:
+            self.squares = [[None for col in range(3)] for row in range(3)]
         else:
-            self.board = board
+            self.squares = squares
 
     def __getitem__(self, coordinate):
         (row, col) = coordinate
-        return self.board[row][col]
+        return self.squares[row][col]
 
     def __setitem__(self, coordinate, value):
         (row, col) = coordinate
-        if self.board[row][col] is not None:
+        if self.squares[row][col] is not None:
             raise ValueError
-        self.board[row][col] = value
+        self.squares[row][col] = value
 
     def do_move(self, move):
         self[move.coordinates] = move.player
@@ -41,12 +41,12 @@ class Board:
     def check_for_game_over(self):
         lines = Board.ROWS + Board.COLUMNS + Board.DIAGONALS
         for line in lines:
-            squares = [self[coords] for coords in line]
-            if all(square is not None for square in squares) and all_equal(squares):
-                player = squares[0]
+            line_squares = [self[coords] for coords in line]
+            if all(square is not None for square in line_squares) and all_equal(line_squares):
+                player = line_squares[0]
                 return Outcome[player.name]
 
-        if all(square is not None for square in flatten(self.board)):
+        if all(square is not None for square in flatten(self.squares)):
             return Outcome.DRAW
 
     def __str__(self):
