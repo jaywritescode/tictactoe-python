6e84eec060ebf02e8a63e6640d118d939f089e2b
Jay Harris <jaywritescode@users.noreply.github.com>
2022-04-30 11:46:06 -0400

    have board apply move to itself rather than move applying itself to board

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -3,12 +3,12 @@ from itertools import cycle
 from src.board import Board
 from src.player import Name
 from src.players.keyboard_interface_player import KeyboardInterfacePlayer
-from src.players.random_choice_player import RandomChoicePlayer
+from src.players.minimax_player import MinimaxPlayer
 
 
 class TicTacToe:
     def __init__(
-        self, playerX_class=KeyboardInterfacePlayer, playerO_class=RandomChoicePlayer
+        self, playerX_class=KeyboardInterfacePlayer, playerO_class=MinimaxPlayer
     ):
         self.board = Board()
         self.playerX = playerX_class(Name.X, self.board)
@@ -23,7 +23,7 @@ class TicTacToe:
 
     def next(self):
         current_player = next(self.players)
-        current_player.get_move().apply(self.board)
+        self.board.do_move(current_player.get_move())
         self.outcome = self.check_for_game_over()
 
     def check_for_game_over(self):
