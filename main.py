d255f2be1eac11c12674cd49af7cbef70d6cd1bf
Jay Harris <jaywritescode@users.noreply.github.com>
2022-05-08 12:36:06 -0400

    include argparse module

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import argparse
 from src.tictactoe import TicTacToe
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="tic-tac-toe w/ minimax decision algorithm")
+    argv = parser.parse_args()
+
     tictactoe = TicTacToe()
     tictactoe.play()
     tictactoe.print()
